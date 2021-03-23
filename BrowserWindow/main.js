b01c5d4fcdceb1da82ae2764ceb966d8145743ec
wsulis <wsulisty64@gmail.com>
2021-03-23 21:03:39 +0800

    update

---
+++ BrowserWindow/main.js
@@ -0,0 +1,28 @@
+console.log('main process working');
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function creatWindow () {
+    win = new BrowserWindow();
+
+}
+
+app.on('ready', creatWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win == null) {
+        creatWindow()
+    }
+});
